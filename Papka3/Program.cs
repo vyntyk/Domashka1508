0ec51054473bae7f97d07d5120919ee00031f761
Viktor Belous <vyntyk@yandex.ru>
2022-08-15 23:09:22 +0300

    Задача 29

---
+++ Papka3/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 29: Напишите программу, которая задаёт
+ массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+Console.Write("Введите размер массива: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+void printArray(int size)
+{
+    int[] arr = new int[size];
+    Console.WriteLine("Введите элементы массива: ");
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.Write("[");
+    for (int i = 0; i < size - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length - 1]}]");
+}
+printArray(N);
